32d6b10dce3cde5966159633d4b1441412d611e4
kimobrian <brian.kimokoti@andela.com>
2017-10-17 19:03:08 +0300

    chore(svc): Bootstrap house service

--- src/app/houses/index.js
+++ src/app/houses/index.js
@@ -1,11 +1,13 @@
 import angular from 'angular';
 import HousesCtrl from './houses.ctrl';
 import HousesComponent from './houses.component'
+import HousesSvc from './houses.svc';
 
 
 let houseApp = angular.module('house', [])
     .controller('HousesCtrl', HousesCtrl)
     .component('houseComponent', HousesComponent)
+    .service('housesSvc', HousesSvc)
     .name;
 
 module.exports = houseApp;
